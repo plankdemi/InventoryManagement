df560289d53e0a8373c6560a93473d3d1ea5d257
plankdemi <baradulinasvladas@gmail.com>
2025-10-16 16:42:00 +0300

    Register and login forms in header

---
+++ InventoryManagement/wwwroot/js/authentication.js
@@ -0,0 +1,23 @@
+﻿
+    const loginView = document.getElementById("loginView");
+    const registerView = document.getElementById("registerView");
+    const modalTitle = document.getElementById("authModalLabel");
+
+    document.addEventListener("click", (e) => {
+        
+        if (e.target.id === "showRegister") {
+            e.preventDefault();
+            loginView.classList.add("d-none");
+            registerView.classList.remove("d-none");
+            modalTitle.textContent = "Register";
+        }
+
+        
+        if (e.target.id === "showLogin") {
+            e.preventDefault();
+            registerView.classList.add("d-none");
+            loginView.classList.remove("d-none");
+            modalTitle.textContent = "Login";
+        }
+    });
+
