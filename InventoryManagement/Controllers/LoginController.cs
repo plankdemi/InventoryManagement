00a25d14bdd9521f1f9a26b372fd42ebe87d4f00
plankdemi <baradulinasvladas@gmail.com>
2025-10-17 22:09:28 +0300

    Login and Register Controllers

---
+++ InventoryManagement/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using InventoryManagement.Data;
+
+
+namespace InventoryManagement.Controllers;
+
+[AllowAnonymous]
+public class LoginController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public LoginController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(string UserName, string Password, bool RememberMe)
+    {
+      
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == UserName && u.Password == Password);
+
+        if (user == null)
+        {
+            ViewBag.Error = "Invalid email or password.";
+            return View();
+        }
+        
+        
+        
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.Username),
+            new Claim("UserId", user.Id.ToString()),
+            
+        };
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var authProperties = new AuthenticationProperties
+        {
+            IsPersistent = RememberMe
+        };
+
+        await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity),
+            authProperties
+        );
+
+        
+        
+        await _context.SaveChangesAsync();
+
+       
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ InventoryManagement/Controllers/RegisterController.cs
@@ -0,0 +1,82 @@
+﻿using InventoryManagement.Models;
+using InventoryManagement.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace InventoryManagement.Controllers
+{
+    [AllowAnonymous]
+    public class RegisterController : Controller
+    {
+        private readonly AppDbContext _context;
+        
+
+        public RegisterController(AppDbContext context)
+        {
+            _context = context;
+            
+        }
+
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+           
+            var existingUser = await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Username == user.Username);
+
+            if (existingUser != null)
+            {
+                ModelState.AddModelError("Email", "This email is already registered.");
+                return View(user);
+            }
+
+            user.CreatedAt = DateTime.UtcNow;
+            
+
+            _context.Users.Add(user);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+
+               
+                
+
+                TempData["SuccessMessage"] = "User registered successfully! Check your email!";
+                return RedirectToAction("Index", "Login");
+            }
+            catch (DbUpdateException ex)
+            {
+                
+                if (ex.InnerException != null && ex.InnerException.Message.Contains("Duplicate entry"))
+                {
+                    ModelState.AddModelError("Email", "This email is already registered.");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "An error occurred while saving. Please try again.");
+                }
+
+                return View(user); 
+            }
+        }
+
+
+    }
+}
