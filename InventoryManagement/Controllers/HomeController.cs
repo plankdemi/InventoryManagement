8cc2d96e74c12eedc4cc1fdf6196c815eed80d98
plankdemi <baradulinasvladas@gmail.com>
2025-10-21 19:42:39 +0300

    prepping homeController

--- InventoryManagement/Controllers/HomeController.cs
+++ InventoryManagement/Controllers/HomeController.cs
@@ -1,17 +1,26 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using InventoryManagement.Models;
+using InventoryManagement.Data;
 
 namespace InventoryManagement.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly AppDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+  
+
+    public HomeController(ILogger<HomeController> logger, AppDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
+    
+    //TODO ADD INVENTORIES SORTED BY CREATEDATE
+    //TODO ADD INVENTORIES SORTED BY NUM OF ITEMS IN INVENTORY
+    //TODO ALL ALL TAGS
 
     public IActionResult Index()
     {
