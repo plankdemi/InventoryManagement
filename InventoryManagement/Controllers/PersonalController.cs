169efe0792fe328f8136d8ce24bed9051f6b0081
plankdemi <baradulinasvladas@gmail.com>
2025-10-24 23:41:32 +0300

    setting up views and controllers

---
+++ InventoryManagement/Controllers/PersonalController.cs
@@ -0,0 +1,16 @@
+﻿using InventoryManagement.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.Controllers;
+
+public class PersonalController:Controller
+{
+    private readonly AppDbContext _context;
+    public PersonalController(AppDbContext context)
+    {
+        
+        _context = context;
+    }
+    
+    //TODO, MOST IMPORTANT WORK ON HOME PAGE > PERSONAL PAGE FIRST > WORK ON INVENTORY PAGE > WORK ON ITEMS PAGE
+}
