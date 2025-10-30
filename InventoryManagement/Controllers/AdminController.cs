15d20cf5c8ed4534e153287082a2d35fb9444c23
plankdemi <baradulinasvladas@gmail.com>
2025-10-30 19:55:39 +0200

    admin page

---
+++ InventoryManagement/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.Controllers;
+
+public class AdminController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
