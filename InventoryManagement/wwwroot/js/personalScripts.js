ebbd5350c28e780a66df38593fee3a14d8fad326
plankdemi <baradulinasvladas@gmail.com>
2025-11-06 23:01:58 +0200

    fix personal view, need to add inventory navigation

--- InventoryManagement/wwwroot/js/personalScripts.js
+++ InventoryManagement/wwwroot/js/personalScripts.js
@@ -3,35 +3,61 @@
     if (!personalTable) return;
 
     const inventories = await fetchInventories();
+    console.log(inventories);
 
     
-    const headerSet = new Set();
+    
+    const headerSets = [];
+    const titles = [];
 
    
     for (const inv of inventories) {
+        const headerSet = new Set();
         for (let i = 1; i <= 3; i++) {
             if (inv[`customSingleStringState${i}`]) headerSet.add(inv[`customSingleStringName${i}`]);
             if (inv[`customMultiStringState${i}`]) headerSet.add(inv[`customMultiStringName${i}`]);
             if (inv[`customIntState${i}`]) headerSet.add(inv[`customIntName${i}`]);
             if (inv[`customBoolState${i}`]) headerSet.add(inv[`customBoolName${i}`]);
             if (inv[`customLinkState${i}`]) headerSet.add(inv[`customLinkName${i}`]);
+            
         }
+        titles.push(inv.title)
+        headerSets.push(headerSet);
     }
+    
+    
+    for (let i = 0; i < headerSets.length; i++) {
+        const set = headerSets[i];
+        if (!set) continue;
 
-    const headers = [...headerSet].filter(Boolean);
-    const headerCells = headers.map(name => `<th scope="col">${name}</th>`).join("");
+        const table = document.createElement("table");
+        table.classList.add("table");
+        
+        const title = document.createElement("h2");
+        title.textContent = titles[i];
+        
+        
+        const headers = [...set].filter(Boolean);
+        const colCount = Math.max(1, headers.length);
+        const headerCells = headers.map(name => `<th scope="col">${name}</th>`).join("");
 
-    personalTable.innerHTML = `
+        table.innerHTML = `
     <thead>
       <tr>${headerCells}</tr>
     </thead>
-    <tbody id="table-newest-body">
-    
-    <td colspan=${headerCells.length}>The inventory is empty...</td>
-
-    
-</tbody>
+    <tbody id="table-newest-body-${i}">
+      <tr>
+        <td colspan="${colCount}">The inventory is empty...</td>
+      </tr>
+    </tbody>
   `;
+        
+        personalTable.appendChild(title);
+        personalTable.appendChild(table);
+    }
+
+
+
 });
 
 async function fetchInventories() {
